575c1cf1c22c312709171894a4f70b8f9f349383
Adam Rosenfield <adam@adamrosenfield.com>
2013-04-19 19:55:55 -0400

    Various changes to test project done by Eclipse

--- test/gen/com/adamrosenfield/wordswithcrosses/wordswithcrosses/test/R.java
+++ test/gen/com/adamrosenfield/wordswithcrosses/wordswithcrosses/test/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.totsp.crossword.shortyz.test;
+package com.adamrosenfield.wordswithcrosses.wordswithcrosses.test;
 
 public final class R {
     public static final class attr {
