3991887a2b5a5311dc328d17500a6ca7274560d5
Adam Rosenfield <adam@adamrosenfield.com>
2013-05-07 00:39:52 -0400

    Suppressed more deprecation warnings

--- wordswithcrosses/src/com/adamrosenfield/wordswithcrosses/PreferencesActivity.java
+++ wordswithcrosses/src/com/adamrosenfield/wordswithcrosses/PreferencesActivity.java
@@ -9,13 +9,13 @@ import android.preference.PreferenceActivity;
 
 import com.adamrosenfield.wordswithcrosses.wordswithcrosses.R;
 
-
 public class PreferencesActivity extends PreferenceActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+        deprecatedAddPreferencesFromResource(R.xml.preferences);
 
-        Preference release = (Preference) findPreference("releaseNotes");
+        Preference release = deprecatedFindPreference("releaseNotes");
         release.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference arg0) {
                     Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("file:///android_asset/release.html"),
@@ -26,7 +26,7 @@ public class PreferencesActivity extends PreferenceActivity {
                 }
             });
 
-        Preference license = (Preference) findPreference("license");
+        Preference license = deprecatedFindPreference("license");
         license.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference arg0) {
                     Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("file:///android_asset/license.html"),
@@ -37,7 +37,7 @@ public class PreferencesActivity extends PreferenceActivity {
                 }
             });
 
-        Preference subscribeNyt = (Preference) findPreference("nytSubscribe");
+        Preference subscribeNyt = deprecatedFindPreference("nytSubscribe");
         subscribeNyt.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference arg0) {
                     Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nytimes.com/puzzle"));
@@ -47,7 +47,7 @@ public class PreferencesActivity extends PreferenceActivity {
                 }
             });
 
-        Preference scrapeInfo = (Preference) findPreference("aboutScrapes");
+        Preference scrapeInfo = deprecatedFindPreference("aboutScrapes");
         scrapeInfo.setOnPreferenceClickListener(new OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference arg0) {
                     Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("file:///android_asset/scrapes.html"),
@@ -58,7 +58,7 @@ public class PreferencesActivity extends PreferenceActivity {
                 }
             });
 
-        Preference sendDebug = (Preference) findPreference("sendDebug");
+        Preference sendDebug = deprecatedFindPreference("sendDebug");
         sendDebug.setOnPreferenceClickListener(new OnPreferenceClickListener(){
 
 			public boolean onPreferenceClick(Preference preference) {
@@ -66,5 +66,15 @@ public class PreferencesActivity extends PreferenceActivity {
 				return true;
 			}
         });
+    }
+
+    @SuppressWarnings("deprecation")
+    private void deprecatedAddPreferencesFromResource(int resource) {
+        addPreferencesFromResource(resource);
+    }
+
+    @SuppressWarnings("deprecation")
+    private Preference deprecatedFindPreference(String preference) {
+        return findPreference(preference);
     }
 }
