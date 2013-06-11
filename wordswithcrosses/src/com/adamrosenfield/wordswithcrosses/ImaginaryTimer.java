19df3d624c0f7c5b22d5a6e9dd49c12bcf8f4a25
Adam Rosenfield <adam@adamrosenfield.com>
2013-06-11 01:55:23 -0400

    Format times over an hour as H:MM:SS instead of MM:SS (with MM potentially over 60)

--- wordswithcrosses/src/com/adamrosenfield/wordswithcrosses/ImaginaryTimer.java
+++ wordswithcrosses/src/com/adamrosenfield/wordswithcrosses/ImaginaryTimer.java
@@ -5,37 +5,51 @@ import java.text.NumberFormat;
 public class ImaginaryTimer {
     private static final long SECONDS = 1000L;
     private static final long MINUTES = 60L * SECONDS;
-    private final NumberFormat format = NumberFormat.getInstance();
+    private static final long HOURS = 60L * MINUTES;
+    private final NumberFormat numberFormat = NumberFormat.getInstance();
     private boolean running = false;
     private long elapsed;
-    private long incept;
+    private long start;
 
     public ImaginaryTimer(long elapsed) {
         this.elapsed = elapsed;
-        this.format.setMinimumIntegerDigits(2);
+        numberFormat.setMinimumIntegerDigits(2);
     }
 
     public long getElapsed() {
-        return this.running ? ((System.currentTimeMillis() - this.incept) + elapsed) : this.elapsed;
+        return running ? ((System.currentTimeMillis() - start) + elapsed) : elapsed;
     }
 
     public void start() {
-        this.incept = System.currentTimeMillis();
+        start = System.currentTimeMillis();
         running = true;
     }
 
     public void stop() {
-        this.elapsed += (System.currentTimeMillis() - this.incept);
+        elapsed += (System.currentTimeMillis() - start);
         running = false;
     }
 
     public String time() {
-        long elapsed = this.getElapsed();
-        long mins = elapsed / MINUTES;
-        long secs = (elapsed - (MINUTES * mins)) / SECONDS;
+        long elapsed = getElapsed();
+        long hours = elapsed / HOURS;
+        long minutes = (elapsed / MINUTES) % 60;
+        long seconds = (elapsed / SECONDS) % 60;
 
-        return new StringBuilder(Long.toString(mins)).append(":")
-                                                     .append(this.format.format(secs))
-                                                     .toString();
+        if (hours > 0) {
+            return new StringBuilder()
+                .append(hours)
+                .append(":")
+                .append(numberFormat.format(minutes))
+                .append(":")
+                .append(numberFormat.format(seconds))
+                .toString();
+        } else {
+            return new StringBuilder()
+                .append(minutes)
+                .append(":")
+                .append(numberFormat.format(seconds))
+                .toString();
+        }
     }
 }
