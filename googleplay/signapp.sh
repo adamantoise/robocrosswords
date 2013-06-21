aed0618c84eb258eefe2ef68c9153ea8e27190b8
Adam Rosenfield <adam@adamrosenfield.com>
2013-06-21 01:19:51 -0400

    Added zipalign step after code signing

--- googleplay/signapp.sh
+++ googleplay/signapp.sh
@@ -6,3 +6,6 @@ if [ $# -lt 1 ]; then
 fi
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.gnupg/wordswithcrosses.keystore "$1" wwc
+
+tempapk="${1%%.apk}-tmp.apk"
+zipalign -f -v 4 "$1" "$tempapk" && mv "$tempapk" "$1"
