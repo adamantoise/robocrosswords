ab20acb6feda31ca2f1a74bb8927aff939c322d0
Adam Rosenfield <adam@adamrosenfield.com>
2013-09-17 21:10:55 -0400

    Back button now closes the whole activity instead of just the native keyboard when using the native keyboard

---
+++ wordswithcrosses/src/com/adamrosenfield/wordswithcrosses/view/CrosswordRelativeLayout.java
@@ -0,0 +1,71 @@
+/**
+ * This file is part of Words With Crosses.
+ *
+ * Copyright (C) 2013 Adam Rosenfield
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.adamrosenfield.wordswithcrosses.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.RelativeLayout;
+
+public class CrosswordRelativeLayout extends RelativeLayout
+{
+    private Activity mActivity;
+
+    public CrosswordRelativeLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        if (context instanceof Activity)
+        {
+            mActivity = (Activity)context;
+        }
+    }
+
+    /**
+     * Intercepts the Back button so that we can close the activity instead of
+     * closing the native keyboard, if that's open
+     */
+    @Override
+    public boolean dispatchKeyEventPreIme(KeyEvent event)
+    {
+        // Based on http://stackoverflow.com/a/5811630/9530
+        if (mActivity != null && event.getKeyCode() == KeyEvent.KEYCODE_BACK)
+        {
+            KeyEvent.DispatcherState state = getKeyDispatcherState();
+            if (state != null)
+            {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0)
+                {
+                    state.startTracking(event, this);
+                    return true;
+                }
+                else if (event.getAction() == KeyEvent.ACTION_UP && !event.isCanceled() && state.isTracking(event))
+                {
+                    mActivity.onBackPressed();
+                    return true;
+                }
+            }
+        }
+
+        return super.dispatchKeyEventPreIme(event);
+    }
+}
